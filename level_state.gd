a443766f961340daf63e3263d3d604b2f9aced7c
Emil Kosz <emssej@gmail.com>
2023-11-21 19:18:30 +0100

    Uh

--- level_state.gd
+++ level_state.gd
@@ -2,8 +2,8 @@ extends Node
 
 var per_level_enemies = {
 	1: [
-		"res://monsters/level 1/amethyst deceiver.tscn",
-		"res://monsters/level 1/spider.tscn"
+		"res://monsters/amethyst deceiver.tscn",
+		"res://monsters/spider.tscn"
 	]
 }
 
