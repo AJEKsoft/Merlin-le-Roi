a71d35cbfb22ab6196e38ba9583c3bef71ca1ede
Adrien Jaguenet <adrien_jaguenet@makelofine.org>
2023-11-21 19:07:45 +0100

    Select runes by dragging

--- runes/rune.gd
+++ runes/rune.gd
@@ -4,6 +4,8 @@ class_name Rune
 
 signal rune_selected(rune:String)
 
+var selected = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$image.scale = Vector2(1, 1)
@@ -22,11 +24,22 @@ func _process(delta):
 func unselect():
 	$selection.visible = false
 	$particles.emitting = false 
+	selected = false
+
+func select():
+	if not selected:
+		selected = true
+		$selection.visible = true
+		$particles.emitting = true
+		emit_signal("rune_selected", self.name)
+
+func _on_mouse_entered():
+	# check if mouse is pressed
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		select()
+
 
 func _on_input_event(viewport:Node, event:InputEvent, shape_idx:int):
 	if event is InputEventMouseButton:
-		if event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT and $selection.visible == false:
-			$selection.visible = true
-			$particles.emitting = true
-			emit_signal("rune_selected", self.name)
-	pass # Replace with function body.
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			select()
