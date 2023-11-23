5be47c812b2b1f9eb9714fe067331d1fa8cee0aa
Adrien Jaguenet <adrien_jaguenet@makelofine.org>
2023-11-23 10:25:22 +0100

    ExitPortal is a grid cell

--- scenes/exploration.gd
+++ scenes/exploration.gd
@@ -20,23 +20,32 @@ func _on_trigger_combat():
 	get_tree().set_current_scene(combat_intro)
 	get_tree().root.remove_child(self)
 
+func get_tile_at(pos : Vector2i):
+	var tile_no = level.find_child("Walls").get_cell_item(Vector3i(pos.x, 0, pos.y))
+	if tile_no == -1:
+		return null
+	else:
+		return level.find_child("Walls").mesh_library.get_item_name(tile_no)
 
 func _on_player_action():
 	print("Player at ", $Player.grid_position, " action on tile ", $Player.get_forward_cell_position())
 	# We check the tile right in front of the player
 	var forward_position = $Player.get_forward_cell_position()
-	forward_position = Vector3i(forward_position.x, 0, forward_position.y)
-	var tile = level.find_child("Walls").get_cell_item(forward_position)
+	var tile = get_tile_at(forward_position)
 	# if the tile is a door, we open it
-	if tile == 3:
+	if tile == "Door":
 		level.find_child("Walls").set_cell_item(forward_position, -1)
 		trigger_combat.emit()
 
 func _on_player_request_move(target : Vector2i):
-	var tile = level.find_child("Walls").get_cell_item(Vector3i(target.x, 0, target.y))
-	if tile == -1:
+	var tile = get_tile_at(target)
+	if tile == null or tile == "ExitPortal":
 		print("Player moving from ", $Player.grid_position, " to ", target)
 		$Player.move_to(target)
 
-func _on_player_enter_tile():
-	pass # Replace with function body.
+func _on_player_enter_tile(tile_pos : Vector2i):
+	var tile = get_tile_at(tile_pos)
+	if tile == "ExitPortal":
+		print("Player entered exit portal")
+		LevelState.current_level += 1
+		get_tree().change_scene_to_file("res://scenes/end_level.tscn")
