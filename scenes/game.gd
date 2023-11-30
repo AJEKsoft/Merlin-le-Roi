46755cbe35753a5d1bb1975046ae92bc54ce3dae
Emil Kosz <emssej@gmail.com>
2023-11-30 21:44:18 +0100

    Fix the cant-click-rune bug, update the UI a bit

--- scenes/game.gd
+++ scenes/game.gd
@@ -3,3 +3,17 @@ extends Control
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _on_player_update_mana(maxv: int, current: int):
+	find_child("player-mana-bar").new_value(current)
+	find_child("player-mana-bar").set_max_value(maxv)
+
+func _on_player_update_health(maxv: int, current: int):
+	find_child("player-health-bar").new_value(current)
+	find_child("player-health-bar").set_max_value(maxv)
+
+func _on_player_damaged(damage: int):
+	find_child("player-health-bar").reduce_value(damage)
+
+func _on_player_lost_mana(value: int):
+	find_child("player-mana-bar").reduce_value(value)
