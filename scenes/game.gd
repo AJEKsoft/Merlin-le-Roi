86342442fda5613dde1d19a37a1515e2aa28419a
Adrien Jaguenet <adrien_jaguenet@makelofine.org>
2023-11-30 09:04:30 +0100

    Fixed rendering after intro

--- scenes/game.gd
+++ scenes/game.gd
@@ -1,9 +1,5 @@
 extends Control 
 
-
-func _ready():
-	self.scale = Vector2(float(get_viewport().size.x) / 854., float(get_viewport().size.y / 480.))
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
