fb3131ab05425947b0bca506a8788cecae11ca59
Adrien Jaguenet <adrien_jaguenet@makelofine.org>
2023-11-30 10:34:29 +0100

    Skip the splash scene with the spacebar

--- scenes/splash.gd
+++ scenes/splash.gd
@@ -1,16 +1,11 @@
 extends Node2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_pressed("skip_scene"):
+		next_scene()
 
+func next_scene():
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 
 func _on_animation_player_animation_finished(anim_name:StringName):
-	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
-	pass # Replace with function body.
+	next_scene()
