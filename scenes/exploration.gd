8407ffe1445d3912af06ba2dbe29f7d58c6a95db
Adrien Jaguenet <adrien_jaguenet@makelofine.org>
2023-12-04 18:02:47 +0100

    Turn-based + decreased particles

--- scenes/exploration.gd
+++ scenes/exploration.gd
@@ -1,6 +1,8 @@
 extends Node3D
 
-var level
+var level : Dungeon
+var turn = "player"
+var turn_blocked = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,41 +11,3 @@ func _ready():
 	level.name = "Dungeon"
 	add_child(level)
 	$Player.place_at(Vector2i(0, 0))
-	$Dungeon.find_child("PlayerRemoteTransform").remote_path = $Player.get_path()
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	var playerfollow = $Dungeon.find_child("PlayerFollow")
-	if playerfollow.progress_ratio < 0.99:
-		playerfollow.progress += delta
-		playerfollow.find_child("PlayerRemoteTransform").position.y = sin(playerfollow.progress * PI * 3) / 50
-
-func get_tile_at(pos : Vector2i):
-	var tile_no = level.find_child("Walls").get_cell_item(Vector3i(pos.x, 0, pos.y))
-	if tile_no == -1:
-		return null
-	else:
-		return level.find_child("Walls").mesh_library.get_item_name(tile_no)
-
-func _on_player_action():
-	print("Player at ", $Player.grid_position, " action on tile ", $Player.get_forward_cell_position())
-	# We check the tile right in front of the player
-	var forward_position = $Player.get_forward_cell_position()
-	var tile = get_tile_at(forward_position)
-	forward_position = Vector3i(forward_position.x, 0, forward_position.y)
-	# if the tile is a door, we open it
-	if tile == "Door":
-		level.find_child("Walls").set_cell_item(forward_position, -1)
-		$DoorSound.play()
-
-func _on_player_request_move(target : Vector2i):
-	var tile = get_tile_at(target)
-	if tile == null or tile == "ExitPortal":
-		print("Player moving from ", $Player.grid_position, " to ", target)
-		$Player.move_to(target)
-
-func _on_player_enter_tile(tile_pos : Vector2i):
-	var tile = get_tile_at(tile_pos)
-	if tile == "ExitPortal":
-		print("Player entered exit portal")
-		get_tree().change_scene_to_file("res://scenes/end_level.tscn")
--- scenes/game.gd
+++ scenes/game.gd
@@ -1,8 +1,19 @@
 extends Control 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+var is_player_turn = true
+var turn_number = 0
+
+signal player_turn_started
+signal player_turn_ended
+signal player_illegal_action
+
+var dungeon : Node3D
+var player : Player
+
+func _ready():
+	dungeon = find_child("Exploration").get_node("Dungeon")
+	player = find_child("Player")
+	player_turn_started.emit()
 
 func _on_player_update_mana(maxv: int, current: int):
 	find_child("player-mana-bar").new_value(current)
@@ -17,3 +28,61 @@ func _on_player_damaged(damage: int):
 
 func _on_player_lost_mana(value: int):
 	find_child("player-mana-bar").reduce_value(value)
+
+
+func _on_turn_timer_timeout():
+	end_player_turn()
+
+func _on_player_turn_started():
+	turn_number += 1
+	print("Turn ", turn_number, " started")
+	$TurnTimer.start()
+	$PlayerTurnStarted.play()
+
+func _on_player_turn_ended():
+	$PlayerTurnEnded.play()
+	#TODO: obviously, this should be replaced by AI actions
+	begin_player_turn()
+
+func _on_player_request_move(target : Vector2i):
+	if not is_player_turn:
+		player_illegal_action.emit()
+		return
+	var tile = dungeon.get_tile_at(target)
+	if tile == null or tile == "ExitPortal":
+		print("Player moving from ", player.grid_position, " to ", target)
+		player.move_to(target)
+
+func _on_player_enter_tile(tile_pos : Vector2i):
+	var tile = dungeon.get_tile_at(tile_pos)
+	if tile == "ExitPortal":
+		print("Player entered exit portal")
+		get_tree().change_scene_to_file("res://scenes/end_level.tscn")
+	end_player_turn()
+
+func _on_player_action():
+	if not is_player_turn:
+		player_illegal_action.emit()
+		return
+	print("Player at ", player.grid_position, " action on tile ", player.get_forward_cell_position())
+	# We check the tile right in front of the player
+	var forward_position = player.get_forward_cell_position()
+	var tile = dungeon.get_tile_at(forward_position)
+	forward_position = Vector3i(forward_position.x, 0, forward_position.y)
+	# if the tile is a door, we open it
+	if tile == "Door":
+		dungeon.find_child("Walls").set_cell_item(forward_position, -1)
+		$DoorSound.play()
+		end_player_turn()
+
+func end_player_turn():
+	print("Turn ended")
+	is_player_turn = false
+	player_turn_ended.emit()
+
+func begin_player_turn():
+	is_player_turn = true
+	player_turn_started.emit()
+
+func _on_player_death():
+	pass # Replace with function body.
